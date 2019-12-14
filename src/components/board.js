231730ce0d92275a7b83e3e3d41d948b3fce8b8e
Елена <tyshenko61@mail.ru>
2019-12-14 20:26:34 +0500

    module4-task1

--- src/components/board.js
+++ src/components/board.js
@@ -1,15 +1,36 @@
-export const createBoardTemplate = () => {
-	return (
-	`<section class="board container">
+import {createElement} from '../utils.js';
+
+const createBoardTemplate = () => {
+  return (
+    `<section class="board container">
         <div class="board__filter-list">
           <a href="#" class="board__filter">SORT BY DEFAULT</a>
           <a href="#" class="board__filter">SORT BY DATE up</a>
           <a href="#" class="board__filter">SORT BY DATE down</a>
         </div>
-        <div class="board__tasks">
-          
-        </div>
-        
+        <div class="board__tasks"></div>
       </section>`
-	);
+  );
+};
+export default class Board {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createBoardTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
 }
+
