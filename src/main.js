e6efe8b47c7008ee70d7d45ae129e142b78ff0b6
Елена <tyshenko61@mail.ru>
2020-01-08 09:49:02 +0500

    Добавлен абстрактный класс

--- src/main.js
+++ src/main.js
@@ -1,50 +1,13 @@
 import BoardComponent from './components/board.js';
+import BoardController from './controllers/board.js';
 import FilterComponent from './components/filter.js';
-import LoadMoreButtonComponent from './components/load-more-button.js';
-import TaskEditComponent from './components/task-edit.js';
-import TaskComponent from './components/task.js';
-import TasksComponent from './components/tasks.js';
-import NoTasksComponent from './components/no-tasks.js';
 import SiteMenuComponent from './components/site-menu.js';
-import SortComponent from './components/sort.js';
 import {generateTasks} from './mock/task.js';
 import {generateFilters} from './mock/filter.js';
-import {render, RenderPosition} from './utils.js';
+import {render, RenderPosition} from './utils/render.js';
 
 const TASK_COUNT = 22;
-const SHOWING_TASKS_COUNT_ON_START = 8;
-const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
-const renderTask = (taskListElement, task) => {
-  const onEscKeyDown = (evt) => {
-    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscKey) {
-      replaceEditToTask();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const replaceEditToTask = () => {
-    taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
-  };
-
-  const replaceTaskToEdit = () => {
-    taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
-  };
-  const taskComponent = new TaskComponent(task);
-
-  const editButton = taskComponent.getElement().querySelector(`.card__btn--edit`);
-  editButton.addEventListener(`click`, () => {
-    replaceTaskToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-  const taskEditComponent = new TaskEditComponent(task);
-  const editForm = taskEditComponent.getElement().querySelector(`form`);
-  editForm.addEventListener(`submit`, replaceEditToTask);
-
-  render(taskListElement, taskComponent.getElement(), RenderPosition.BEFOREEND);
-};
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
@@ -52,37 +15,12 @@ render(siteHeaderElement, new SiteMenuComponent().getElement(), RenderPosition.B
 
 const filters = generateFilters();
 render(siteMainElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
+
 const boardComponent = new BoardComponent();
 render(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
 
 const tasks = generateTasks(TASK_COUNT);
-const isAllTasksArchived = tasks.every((task) => task.isArchive);
-if (isAllTasksArchived) {
-  render(boardComponent.getElement(), new NoTasksComponent().getElement(), RenderPosition.BEFOREEND);
-} else {
-  render(boardComponent.getElement(), new SortComponent().getElement(), RenderPosition.BEFOREEND);
-  render(boardComponent.getElement(), new TasksComponent().getElement(), RenderPosition.BEFOREEND);
 
-  const taskListElement = boardComponent.getElement().querySelector(`.board__tasks`);
+const boardController = new BoardController(boardComponent);
 
-  let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
-  tasks.slice(0, showingTasksCount)
-    .forEach((task) => {
-      renderTask(taskListElement, task);
-    });
-  const loadMoreButtonComponent = new LoadMoreButtonComponent();
-  render(boardComponent.getElement(), loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
-  loadMoreButtonComponent.getElement().addEventListener(`click`, () => {
-    const prevTasksCount = showingTasksCount;
-    showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
-
-    tasks.slice(prevTasksCount, showingTasksCount)
-      .forEach((task) => renderTask(task));
-
-    if (showingTasksCount >= tasks.length) {
-      loadMoreButtonComponent.getElement().remove();
-      loadMoreButtonComponent.removeElement();
-    }
-  });
-}
-
+boardController.render(tasks);
--- src/mock/task.js
+++ src/mock/task.js
@@ -1,81 +1,77 @@
 import {Colors} from '../constant.js';
 
 const DescriptionItems = [
-	`Изучить теорию`,
-	`Сделать домашку`,
-	`Пройти интенсив на соточку`,
+  `Изучить теорию`,
+  `Сделать домашку`,
+  `Пройти интенсив на соточку`,
 ];
 
 const DefaultRepeatingDays = {
-	'mo': false,
-	'tu': false,
-	'we': false,
-	'th': false,
-	'fr': false,
-	'sa': false,
-	'su': false,
+  'mo': false,
+  'tu': false,
+  'we': false,
+  'th': false,
+  'fr': false,
+  'sa': false,
+  'su': false,
 };
 
 const Tags = [
-		`homework`,
-		`theory`,
-		`practice`,
-		`intensive`,
-		`keks`
+  `homework`,
+  `theory`,
+  `practice`,
+  `intensive`,
+  `keks`
 ];
 
 const getRandomArrayItem = (array) => {
-	const randomIndex = getRandomIntegerNumber(0, array.length);
-	
-	return array[randomIndex];
+  const randomIndex = getRandomIntegerNumber(0, array.length);
+
+  return array[randomIndex];
 };
 
 const getRandomIntegerNumber = (min, max) => {
-	return min + Math.floor(max * Math.random());
+  return min + Math.floor(max * Math.random());
 };
 
 const getRandomDate = () => {
-	const targetDate = new Date();
-	const sign = Math.random() > 0.5 ? 1: -1;
-	const diffValue = sign * getRandomIntegerNumber(0,7);
-	
-	targetDate.setDate(targetDate.getDate() + diffValue);
-	
-	return targetDate;
+  const targetDate = new Date();
+  const sign = Math.random() > 0.5 ? 1 : -1;
+  const diffValue = sign * getRandomIntegerNumber(0, 7);
+
+  targetDate.setDate(targetDate.getDate() + diffValue);
+
+  return targetDate;
 };
 
-const generateReppeatingDays=() => {
-	return Object.assign({}, DefaultRepeatingDays, {
-		'mo': Math.random() > 0.5,
-	});
+const generateReppeatingDays = () => {
+  return Object.assign({}, DefaultRepeatingDays, {
+    'mo': Math.random() > 0.5,
+  });
 };
 
 const generateTags = (tags) => {
-	return tags.filter(() => Math.random() > 0.5).slice(0,3);
+  return tags.filter(() => Math.random() > 0.5).slice(0, 3);
 };
 
 const generateTask = () => {
-	const dueDate = Math.random() >0.5 ? null:getRandomDate();
-	
-	return {
-		description : getRandomArrayItem(DescriptionItems),
-		dueDate,
-		repeatingDays: dueDate? DefaultRepeatingDays:generateReppeatingDays(),
-		tags: new Set(generateTags(Tags)),
-		color: getRandomArrayItem(Colors),
-		isFavorite: Math.random()>0.5,
-		isArchive: Math.random()>0.5,
-	};
+  const dueDate = Math.random() > 0.5 ? null : getRandomDate();
+
+  return {
+    description: getRandomArrayItem(DescriptionItems),
+    dueDate,
+    repeatingDays: dueDate ? DefaultRepeatingDays : generateReppeatingDays(),
+    tags: new Set(generateTags(Tags)),
+    color: getRandomArrayItem(Colors),
+    isFavorite: Math.random() > 0.5,
+    isArchive: Math.random() > 0.5,
+  };
 };
 
 const generateTasks = (count) => {
-	return new Array(count).fill(``).map(generateTask);
+  return new Array(count).fill(``).map(generateTask);
 };
 
 export {generateTask, generateTasks};
 
 
-
-	
-	
-	
