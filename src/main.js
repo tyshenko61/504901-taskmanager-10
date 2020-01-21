cb0dbd184e0ce221faeda16ac0339b20d9ced3f9
Елена <tyshenko61@mail.ru>
2020-01-21 21:49:36 +0500

    module7-task1

--- src/main.js
+++ src/main.js
@@ -1,26 +1,32 @@
 import BoardComponent from './components/board.js';
 import BoardController from './controllers/board.js';
-import FilterComponent from './components/filter.js';
+import FilterController from './controllers/filter.js';
 import SiteMenuComponent from './components/site-menu.js';
+import TasksModel from './models/tasks.js';
 import {generateTasks} from './mock/task.js';
-import {generateFilters} from './mock/filter.js';
+// import {generateFilters} from './mock/filter.js';
 import {render, RenderPosition} from './utils/render.js';
 
 const TASK_COUNT = 22;
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const siteMenuComponent = new SiteMenuComponent();
+const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
+siteMenuComponent.getElement().querySelector(`.control__label--new-task`)
+  .addEventListener(`click`, () => {
+    boardController.createTask();
+  });
 
-render(siteHeaderElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
+render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
 
-const filters = generateFilters();
-render(siteMainElement, new FilterComponent(filters), RenderPosition.BEFOREEND);
-
+const filterController = new FilterController(siteMainElement, tasksModel);
+filterController.render();
 const boardComponent = new BoardComponent();
 render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
 
-const tasks = generateTasks(TASK_COUNT);
+const boardController = new BoardController(boardComponent, tasksModel);
 
-const boardController = new BoardController(boardComponent);
-
-boardController.render(tasks);
+boardController.render();
